f7db8ebd0a19ea716d1e80528d4297ed89931a4f
wryk <wryk@wryk.moe>
2019-08-29 19:16:32 +0200

    simplify profile form

--- src/Page/Home.purs
+++ src/Page/Home.purs
@@ -20,13 +20,10 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 
 type State =
-    { username :: Maybe Username
-    }
+    Unit
 
 data Action
-    = Initialize
-    | Demo
-    | HandleForm FormFields
+    = HandleForm FormFields
 
 type Query a
     = Const Void
@@ -38,7 +35,7 @@ type Output
     = Void
 
 type ChildSlots =
-  ( formless :: F.Slot ProfileForm FormQuery () FormFields Unit
+  ( formless :: F.Slot ProfileForm (Const Void) () FormFields Unit
   )
 
 component
@@ -51,49 +48,23 @@ component = H.mkComponent
     , render
     , eval: H.mkEval $ H.defaultEval
         { handleAction = handleAction
-        , initialize = Just Initialize
         }
     }
 
     where
         initialState :: Input -> State
-        initialState _ =
-            { username: Nothing
-            }
+        initialState _ = unit
 
         handleAction :: Action -> H.HalogenM State Action ChildSlots Void m Unit
         handleAction = case _ of
-            Initialize -> do
-                H.modify_ _ { username = Just $ Username "tixie" }
-
             HandleForm fields -> do
                 H.liftEffect $ logShow fields
-
-            Demo -> do
-                maybeUsername <- H.gets _.username
-
-                case maybeUsername of
-                    Nothing ->
-                        pure unit
-                    Just username ->
-                        navigate $ Route.Profile username
+                navigate $ Route.Profile fields.username
 
         render :: State -> H.ComponentHTML Action ChildSlots m
         render state =
             HH.div_
                 [ HH.h1_ [ HH.text "Home" ]
-                , HH.input
-                    [ HP.type_ HP.InputText
-                    , HP.value $ maybe "" unwrap state.username
-                    , HP.readOnly true
-                    ]
-                , HH.button
-                    [ HP.type_ HP.ButtonButton
-                    , HP.disabled $ isNothing state.username
-                    , HE.onClick \_ -> Just Demo
-                    ]
-                    [ HH.text "Go to profile"
-                    ]
                 , HH.slot F._formless unit formComponent unit (Just <<< HandleForm)
                 ]
 
@@ -108,47 +79,30 @@ newtype ProfileForm r f = ProfileForm (r
 
 derive instance newtypeProfileForm :: Newtype (ProfileForm r f) _
 
-data FormQuery a
-  = SetLoginError Boolean a
-
-derive instance functorFormQuery :: Functor (FormQuery)
-
-formComponent :: ∀ m. MonadAff m => F.Component ProfileForm FormQuery () Unit FormFields m
+formComponent :: ∀ m. MonadAff m => F.Component ProfileForm (Const Void) () Unit FormFields m
 formComponent = F.component formInput $ F.defaultSpec
-    { render = renderLogin
-    , handleEvent = handleEvent
-    , handleQuery = handleQuery
+    { render = renderForm
+    , handleEvent = F.raiseResult
     }
     where
-        formInput :: Unit -> F.Input ProfileForm (formError :: Boolean) m
+        formInput :: Unit -> F.Input' ProfileForm m
         formInput _ =
             { validators: ProfileForm
                 { username: V.required >>> V.usernameFormat
                 }
             , initialInputs: Nothing
-            , formError: false
             }
 
-        handleEvent = F.raiseResult
-
-        handleQuery :: ∀ a. FormQuery a -> H.HalogenM _ _ _ _ _ (Maybe a)
-        handleQuery = case _ of
-            SetLoginError bool a -> do
-                H.modify_ _ { formError = bool }
-                pure (Just a)
 
         proxies = F.mkSProxies (F.FormProxy :: _ ProfileForm)
 
-        renderLogin { form, formError } =
+        renderForm { form } =
             HH.form_
-                [ whenElement formError \_ ->
-                    HH.div_
-                        [ HH.text "Error" ]
-                    , HH.fieldset_
-                        [ Field.input proxies.username form
-                            [ HP.placeholder "Username"
-                            , HP.type_ HP.InputText
-                            ]
-                        , Field.submit "Go to profile"
+                [ HH.fieldset_
+                    [ Field.input proxies.username form
+                        [ HP.placeholder "Username"
+                        , HP.type_ HP.InputText
+                        ]
+                    , Field.submit "Go to profile"
                     ]
                 ]
